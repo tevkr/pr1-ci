334087009c59e974645c4d82ffed9cd041995b09
nom <vlad.nomerov@yandex.ru>
2024-02-18 01:23:26 +0300

    insertionSort added to SortingAlgorithms; Travis CI yml added to project

--- src/main/java/org/example/SortingAlgorithms.java
+++ src/main/java/org/example/SortingAlgorithms.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class SortingAlgorithms {
+    private SortingAlgorithms() {
+        throw new IllegalStateException("Utility class");
+    }
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
@@ -11,5 +14,20 @@ public class SortingAlgorithms {
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
+    }
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
--- src/test/java/SortingAlgorithmsTest.java
+++ src/test/java/SortingAlgorithmsTest.java
@@ -2,12 +2,19 @@ import org.example.SortingAlgorithms;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class SortingAlgorithmsTest {
+class SortingAlgorithmsTest {
     @Test
-    public void testBubbleSort() {
+    void testBubbleSort() {
         int[] actual = {5, 1, 4, 2, 8};
         int[] expected = {1, 2, 4, 5, 8};
         SortingAlgorithms.bubbleSort(actual);
         assertArrayEquals(expected, actual, "The bubble sort did not sort the array correctly.");
+    }
+    @Test
+    void testInsertionSort() {
+        int[] actual = {3, 1, 2, 5, 7, 0};
+        int[] expected = {0, 1, 2, 3, 5, 7};
+        SortingAlgorithms.insertionSort(actual);
+        assertArrayEquals(expected, actual, "The insertion sort did not sort the array correctly.");
     }
 }
